9ef451d0fcbe4072a0e8d1a3149ea5a4120a3b77
HussainQH <hussainqh@gmail.com>
2021-10-04 20:51:40 +0300

    Completed Task

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,12 @@
 import React from "react";
 // Styling
 
-
-const SearchBar = (props) => {
-  return (
-    <input className="searchBar"
-      onChange={(event) => console.log(event.target.value)}
-      placeholder="Search for a cookie name"
-    />
-  );
-};
+const SearchBar = (props) => (
+  <input
+    className="searchBar"
+    onChange={(event) => props.setQuery(event.target.value)}
+    placeholder="Search for a cookie name"
+  />
+);
 
 export default SearchBar;
